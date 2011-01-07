719e3a1612ac21a37116b3db0dbea4babfdd21aa
Alex Murray <murray.alex@gmail.com>
2011-01-07 16:40:45 +1030

    Initial commit of emacs configs

---
+++ .emacs
@@ -0,0 +1,186 @@
+;;;; Standard Emacs options and inbuilt packages ;;;;
+
+;; set visual properties early to reduce frame flickering
+;; set default font properties
+(set-face-attribute 'default nil :font "DejaVu Sans Mono")
+(set-face-attribute 'default nil :height 90)
+;; disable tool-bar and scroll-bar, show matching parenthesis and time
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+(show-paren-mode 1)
+(auto-fill-mode 1)
+(display-time)
+
+;; default to utf-8
+(set-language-environment "UTF-8")
+
+;; some nice keybindings
+(global-set-key "\C-x\C-h" 'hexl-mode)
+(global-set-key "\C-x\C-m" 'compile)
+(global-set-key "\C-xe" 'shell)
+
+;; Show line column numbers in mode line
+(line-number-mode t)
+(column-number-mode t)
+
+;; show colours correctly in shell
+(ansi-color-for-comint-mode-on)
+
+;; delete trailing whitespace on save
+(add-hook 'before-save-hook (lambda () (delete-trailing-whitespace)))
+
+;; always end a file with a newline
+(setq require-final-newline 'query)
+
+;; use proper english
+(setq ispell-dictionary "british")
+
+;; inhibit startup message and splash screen
+(setq inhibit-startup-message t)
+
+;; default to unified diff
+(setq diff-switches "-u")
+
+;; since we store .emacs in a symlinked git repo, always follow symlinks for vc
+(setq vc-follow-symlinks t)
+
+;; remember recently used files
+(require 'recentf)
+(recentf-mode 1)
+
+;; ido mode for opening files and switching buffers
+(ido-mode 1)
+
+;; for org-mode
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+;; semantic in emacs 23.2 - useful for auto-complete mode
+(require 'semantic)
+(semantic-mode 1)
+
+;;;; External packages ;;;;
+
+;; set load-path
+(add-to-list 'load-path (expand-file-name "~/.emacs.d"))
+
+;; devhelp
+(require 'devhelp
+	 ;; Bind F6 to enable the automatic assistant.
+	 (global-set-key [f6] 'devhelp-toggle-automatic-assistant)
+	 ;; Bind F7 to search with the assistant window.
+	 (global-set-key [f7] 'devhelp-assistant-word-at-point)
+	 )
+
+;; color theme - requires emacs-goodies-el to be installed on ubuntu
+(autoload 'color-theme-blackboard "color-theme-blackboard" "blackboard color theme." t)
+(color-theme-blackboard)
+
+;; auto-complete mode
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+
+;; rainbow mode - for colouring strings that represent colors
+(autoload 'rainbow-mode "rainbow-mode" "rainbow-mode." t)
+(rainbow-mode 1)
+
+;; code snippet support
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/yasnippet-0.6.1c"))
+(require 'yasnippet)
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/yasnippet-0.6.1c/snippets")
+
+;; undo-tree.el - from http://www.dr-qubit.org/undo-tree/undo-tree.el
+(require 'undo-tree)
+(global-undo-tree-mode 1)
+
+;; scratch.el - from http://github.com/ieure/scratch-el
+(autoload 'scratch "scratch" nil t)
+
+;;;; Support for specific languages ;;;;
+
+;; for auctex and reftex integration
+(add-hook 'LaTeX-mode-hook
+	  (lambda ()
+	    ;; Enable source-specials for Control-click forward/reverse search.
+	    (TeX-source-specials-mode 1)
+	    (setq TeX-source-specials-view-start-server t)
+	    ;; Enable reftex
+	    (turn-on-reftex)
+	    (setq reftex-plug-into-AUCTeX t)
+	    ;; Enable flyspell
+	    (flyspell-mode 1)
+	    ;; workaround bug in autocomplete and flyspell
+	    (ac-flyspell-workaround)
+	    )
+	  )
+
+;; C coding etc
+(add-hook 'c-mode-common-hook
+          (lambda ()
+	    ;; use linux kernel style
+	    (c-set-style "linux")
+	    ;; use tabs not spaces to indent
+	    (setq indent-tabs-mode t)
+	    ;; options for c-eldoc mode - could also add other libs too like OpenGL if
+	    ;; needed
+	    (setq c-eldoc-includes "`pkg-config gtk+-2.0 --cflags` -I./ -I../ ")
+	    (load "c-eldoc")
+	    ;; turn on auto-newline and hungry-delete
+	    (c-toggle-auto-hungry-state t)
+	    ;; show trailing whitespace
+	    (setq show-trailing-whitespace t)
+	    ;; set auto newline
+	    (setq c-auto-newline 1)
+	    (setq fill-column 80)
+	    (setq comment-column 60)
+	    ;; turn on spell checking for strings and comments
+	    (flyspell-prog-mode)
+	    ;; workaround bug in autocomplete and flyspell
+	    (ac-flyspell-workaround)
+	    ;; make CamelCase words separate subwords (ie. Camel and Case can
+	    ;; be operated on separately as separate words
+	    (subword-mode 1)
+	    ;; turn on c-eldoc
+	    (c-turn-on-eldoc-mode)
+	    ;; enable gtk-doc helpers from gtk-doc-tools to easily
+	    ;; insert gtk-doc style comment declarations using C-x 4 h
+	    ;; (gtk-doc-insert) or C-x 4 s (gtk-doc-insert-section) to
+	    ;; comment current function or section respectively
+	    (load "gtk-doc")
+	    ;; use semantic as a source for auto complete
+	    (setq ac-sources (append ac-sources '(ac-source-semantic)))
+	    )
+	  )
+
+;; for C code highlight TODO and fixme so it looks scary
+(font-lock-add-keywords
+ 'c-mode
+ '(("\\<\\(TODO\\|todo\\|FIXME\\|fixme\\)" 1 font-lock-warning-face t)))
+
+;; android-mode
+(require 'android-mode)
+(setq android-mode-sdk-dir "~/android-sdk-linux_x86/")
+(add-hook 'gud-mode-hook
+	  (lambda ()
+            (add-to-list 'gud-jdb-classpath "/home/alex/android-sdk-linux_86/platforms/android-8/android.jar")
+            ))
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(ecb-options-version "2.32")
+ '(user-full-name "Alex Murray")
+ '(user-mail-address "murray.alex@gmail.com")
+ '(x-select-enable-clipboard t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
---
+++ .emacs.d/gobject-class.el
@@ -0,0 +1,476 @@
+;;; gobject-class.el --- functions to easy GObject-based class developers
+;; Author: Gustavo Sverzut Barbieri <barbieri@gmail.com>
+;; Author: Youness Alaoui <youness.alaoui@collabora.co.uk>
+;; Copyright: public domain
+;; URL: http://blog.gustavobarbieri.com.br/old-website/gobject-class.el
+;; EmacsWiki: GObjectClassHelpers
+;; Keywords: gobject, glib, gtk, helper, utilities
+;;; Commentary:
+;;; Code:
+
+
+(defun string-join (list separator)
+  "Takes a list of string and joins them using delimiter."
+  (mapconcat (lambda (x) x) list separator))
+
+(defun string-concat (list)
+  "Takes a list of strings and joins them."
+  (mapconcat (lambda (x) x) list ""))
+
+(defun ask-value-non-empty (prompt)
+  "Ask a question in minibuffer and ensure it's not empty string."
+  (let ((x (read-string prompt)))
+    (if (string= "" x)
+	(ask-value-non-empty prompt)
+      x)))
+
+(defun gobject-class-header (class_name parent_class_name private)
+  "Generate the header definition for a GObject derived class.
+
+It takes 2 parameters:
+   CLASS_NAME: class name, like 'gtk_button' or 'gtk_tree_view'. First
+      element before the underscore character (_) will be used as name
+      space. Example: 'gtk_button' is the 'button' class in 'gtk' name
+      space.
+   PARENT_CLASS_NAME: parent class name, like 'g_object'. First element
+      before the underscore character (_) will be used as name space.
+      Example: 'g_object' is the 'object' class in 'g' namespace.
+   PRIVATE: Whether or not to include a Private structure and priv field.
+"
+  (interactive "sClass name (ie: gtk_tree_view): \nsParent class name (default: g_object): \nsAdd Private structure ([y]/n)?\n")
+
+  (defun right-fill (len string)
+    "Takes a string and fill it to take at least len characters"
+    (setq missing (- len (length string)))
+    (if	(> missing 0)
+	(concat string (make-string missing ?\  ))
+      string))
+  (defun macro-line-cnt (&rest sequences)
+    (concat (right-fill 72 (string-concat sequences)) "\\\n"))
+  (defun macro-line-end (&rest sequences)
+    (concat (string-concat sequences) "\n"))
+
+
+  (let* ((parent_class_name (if (string= "" parent_class_name)
+				"g_object"
+			      (downcase parent_class_name)))
+	 (class_name        (downcase (if (string= "" class_name)
+					  (ask-value-non-empty
+					   "You must provide class name (ie: gtk_tree_view): ")
+					class_name)))
+	 (private (if (string= "n" private) "n" "y"))
+	 (pieces-class_name  (split-string (downcase class_name) "_"))
+	 (pieces-parent_class_name (split-string parent_class_name "_"))
+	 (namespace       (car-safe pieces-class_name))
+	 (name            (string-join (cdr-safe pieces-class_name) "_"))
+	 (ParentClassName (mapconcat 'capitalize pieces-parent_class_name ""))
+	 (NAMESPACE       (upcase namespace))
+	 (NAME            (upcase name))
+	 (NameSpace       (capitalize namespace))
+	 (Name            (mapconcat 'capitalize (cdr-safe pieces-class_name) ""))
+	 (ClassName       (concat NameSpace Name))
+	 )
+
+    (insert
+     (concat
+      "\n"
+      "G_BEGIN_DECLS\n"
+      "\n"
+      (macro-line-cnt "#define " NAMESPACE "_TYPE_" NAME)
+      (macro-line-end "   (" namespace "_" name "_get_type())")
+      (macro-line-cnt "#define " NAMESPACE "_" NAME "(obj)")
+      (macro-line-cnt "   (G_TYPE_CHECK_INSTANCE_CAST ((obj),")
+      (macro-line-cnt "                                " NAMESPACE "_TYPE_" NAME ",")
+      (macro-line-end "                                " ClassName "))")
+      (macro-line-cnt "#define " NAMESPACE "_" NAME "_CLASS(klass)")
+      (macro-line-cnt "   (G_TYPE_CHECK_CLASS_CAST ((klass),")
+      (macro-line-cnt "                             " NAMESPACE "_TYPE_" NAME ",")
+      (macro-line-end "                             " ClassName "Class))")
+      (macro-line-cnt "#define IS_" NAMESPACE "_" NAME "(obj)")
+      (macro-line-cnt "   (G_TYPE_CHECK_INSTANCE_TYPE ((obj),")
+      (macro-line-end "                                " NAMESPACE "_TYPE_" NAME "))")
+      (macro-line-cnt "#define IS_" NAMESPACE "_" NAME "_CLASS(klass)")
+      (macro-line-cnt "   (G_TYPE_CHECK_CLASS_TYPE ((klass),")
+      (macro-line-end "                             " NAMESPACE "_TYPE_" NAME "))")
+      (macro-line-cnt "#define " NAMESPACE "_" NAME "_GET_CLASS(obj)")
+      (macro-line-cnt "   (G_TYPE_INSTANCE_GET_CLASS ((obj),")
+      (macro-line-cnt "                               " NAMESPACE "_TYPE_" NAME ",")
+      (macro-line-end "                               " ClassName "Class))")
+      "\n"
+      "typedef struct _" ClassName "      " ClassName ";\n"
+      "typedef struct _" ClassName "Class " ClassName "Class;\n"
+      (if (string= "y" private) 
+	  (concat "typedef struct _" ClassName "Private " ClassName "Private;\n")
+	"")
+	  
+      "\n"
+      "struct _" ClassName "Class\n"
+      "{\n"
+      "    " ParentClassName "Class parent_class;\n"
+      "};\n"
+      "\n"
+      "struct _" ClassName "\n"
+      "{\n"
+      "    " ParentClassName " parent;\n"
+      (if (string= "y" private) 
+	  (concat "    " ClassName "Private *priv;\n")
+	  "")
+      "};\n"
+      "\n"
+      "GType " namespace "_" name "_get_type (void) G_GNUC_CONST;\n"
+      "\n"
+      "G_END_DECLS\n"
+      "\n"
+      )
+     )
+    )
+  )
+
+(defun gobject-class-code (class_name parent_class_name private properties signals)
+  "Generate the code implementation for a GObject derived class.
+
+It takes 2 parameters:
+   CLASS_NAME: class name, like 'gtk_button' or 'gtk_tree_view'. First
+      element before the underscore character (_) will be used as name
+      space. Example: 'gtk_button' is the 'button' class in 'gtk' name
+      space.
+   PARENT_CLASS_NAME: parent class name, like 'g_object'. First element
+      before the underscore character (_) will be used as name space.
+      Example: 'g_object' is the 'object' class in 'g' namespace.
+   PRIVATE: Whether or not to include a Private structure and priv field.
+   PROPERTIES: Whether or not to include properties.
+   SIGNALS: Whether or not to include signals.
+"
+  (interactive "sClass name (ie: gtk_tree_view): \nsParent class name (default: g_object): \nsAdd Private structure ([y]/n)?\nsAdd properties ([y]/n)?\nsAdd signals ([y]/n)?\n")
+
+  (let* ((parent_class_name (if (string= "" parent_class_name)
+				"g_object"
+			      (downcase parent_class_name)))
+	 (class_name        (downcase (if (string= "" class_name)
+					  (ask-value-non-empty
+					   "You must provide class name (ie: gtk_tree_view): ")
+					class_name)))
+	 (private (if (string= "n" private) "n" "y"))
+	 (properties (if (string= "n" properties) "n" "y"))
+	 (signals (if (string= "n" signals) "n" "y"))
+	 (pieces-class_name  (split-string (downcase class_name) "_"))
+	 (pieces-parent_class_name (split-string parent_class_name "_"))
+	 (namespace       (car-safe pieces-class_name))
+	 (name            (string-join (cdr-safe pieces-class_name) "_"))
+	 (ParentClassName (mapconcat 'capitalize pieces-parent_class_name ""))
+	 (NameSpace       (capitalize namespace))
+	 (Name            (mapconcat 'capitalize (cdr-safe pieces-class_name) ""))
+	 (ClassName       (concat NameSpace Name))
+	 (class_name      (concat namespace "_" name))
+	 (NAMESPACE       (upcase namespace))
+	 (NAME            (upcase name))
+         (PARENT_NAMESPACE (upcase (car-safe pieces-parent_class_name)))
+         (PARENT_NAME      (mapconcat 'upcase (cdr-safe pieces-parent_class_name) "_"))
+	 )
+
+    (insert
+     (concat
+      "\n"
+      "G_DEFINE_TYPE (" ClassName ", " class_name ", " PARENT_NAMESPACE "_TYPE_" PARENT_NAME ");\n"
+      "\n"
+      "static void " class_name "_constructed (GObject *object);\n"
+      "static void " class_name "_dispose (GObject *object);\n"
+      "static void " class_name "_finalize (GObject *object);\n"
+      (if (string= "y" properties) 
+	  (concat "static void " class_name "_get_property (GObject *object,\n"
+		  "   guint property_id, GValue *value, GParamSpec *pspec);\n"
+		  "static void " class_name "_set_property (GObject *object,\n"
+		  "   guint property_id, const GValue *value, GParamSpec *pspec);\n")
+	"")
+      "\n"
+      (if (string= "y" signals) 
+	  (concat   "/* signal enum */\n"
+		    "enum {\n"
+		    "    SIGNAL_DUMMY,\n"
+		    "    LAST_SIGNAL\n"
+		    "};\n"
+		    "\n"
+		    "static guint signals[LAST_SIGNAL] = {0};\n"
+		    "\n")
+	"")
+
+      (if (string= "y" properties) 
+	  (concat      "/* properties */\n"
+		       "enum {\n"
+		       "  PROP_DUMMY = 1,\n"
+		       "  LAST_PROPERTY\n"
+		       "};\n")
+	"")
+      "\n"
+      (if (string= "y" private) 
+	  (concat    "struct _" ClassName "Private\n"
+		     "{\n"
+		     "  gboolean dispose_has_run;\n"
+		     "};\n"
+		     "\n")
+	"")
+      "static void\n"
+      class_name "_class_init (" ClassName "Class *klass)\n"
+      "{\n"
+      "    GObjectClass *gobject_class = G_OBJECT_CLASS (klass);\n"
+      "\n"
+      (if (string= "y" private) 
+	  (concat  "    g_type_class_add_private (klass, sizeof (" ClassName "Private));\n"
+		   "\n")
+	"")
+      (if (string= "y" properties) 
+	  (concat       "    gobject_class->get_property = " class_name "_get_property;\n"
+			"    gobject_class->set_property = " class_name "_set_property;\n")
+	"")
+      "    gobject_class->constructed = " class_name "_constructed;\n"
+      "    gobject_class->dispose = " class_name "_dispose;\n"
+      "    gobject_class->finalize = " class_name "_finalize;\n"
+      "\n"
+      (if (string= "y" properties) 
+	  (concat   "    g_object_class_install_property (gobject_class, PROP_DUMMY,\n"
+		    "        g_param_spec_uint (\"dummy\", \"dummy property\",\n"
+		    "            \"dummy property blurp.\",\n"
+		    "            0, G_MAXUINT,\n"
+		    "            0,\n"
+		    "            G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));\n"
+		    "\n")
+	"")
+      (if (string= "y" properties) 
+	  (concat  "    signals[SIGNAL_DUMMY] = g_signal_new (\"dummy\",\n"
+		   "        G_OBJECT_CLASS_TYPE (klass),\n"
+		   "        G_SIGNAL_RUN_LAST | G_SIGNAL_DETAILED,\n"
+		   "        0,\n"
+		   "        NULL, NULL,\n"
+		   "        g_cclosure_marshal_VOID__VOID,\n"
+		   "        G_TYPE_NONE, 0);\n"
+		   "\n")
+	"")
+      "}\n"
+      "\n"
+      "static void\n"
+      class_name "_init (" ClassName " *self)\n"
+      "{\n"
+      (if (string= "y" private) 
+	  (concat  "    " ClassName "Private *priv =\n"
+		   "        G_TYPE_INSTANCE_GET_PRIVATE (self, " NAMESPACE "_TYPE_" NAME ",\n"
+		   "            " ClassName "Private);\n"
+		   "\n"
+		   "    self->priv = priv;\n"
+		   "    priv->dispose_has_run = FALSE;\n")
+	"")
+      "}\n"
+      "\n"
+      (if (string= "y" properties) 
+	  (concat     "static void\n"
+		      class_name "_get_property (GObject *object,\n"
+		      "  guint property_id, GValue *value, GParamSpec *pspec)\n"
+		      "{\n"
+		      "    " ClassName" *self = " NAMESPACE "_" NAME " (object);\n"
+		      (if (string= "y" private) 
+			  (concat   "    " ClassName "Private *priv = self->priv;\n"
+				    "\n"
+				    "    /* Make compiler happy */\n"
+				    "    (void)priv;\n")
+			"")
+		      "\n"
+		      "    switch (property_id) {\n"
+		      "      case PROP_DUMMY:\n"
+		      "        g_value_set_uint (value, 0);\n"
+		      "        break;\n"
+		      "      default:\n"
+		      "        G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);\n"
+		      "        break;\n"
+		      "    }\n"
+		      "}\n"
+		      "\n"
+		      "static void\n"
+		      class_name "_set_property (GObject *object,\n"
+		      "  guint property_id, const GValue *value, GParamSpec *pspec)\n"
+		      "{\n"
+		      "    " ClassName" *self = " NAMESPACE "_" NAME " (object);\n"
+		      (if (string= "y" private) 
+			  (concat   "    " ClassName "Private *priv = self->priv;\n"
+				    "\n"
+				    "    /* Make compiler happy */\n"
+				    "    (void)priv;\n")
+			"")
+		      "\n"
+		      "    switch (property_id) {\n"
+		      "      case PROP_DUMMY:\n"
+		      "        g_value_get_uint (value);\n"
+		      "        break;\n"
+		      "      default:\n"
+		      "        G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);\n"
+		      "        break;\n"
+		      "    }\n"
+		      "}\n")
+	"")
+      "\n"
+      "static void\n"
+      class_name "_constructed (GObject *object)\n"
+      "{\n"
+      "    void (*chain_up) (GObject *) =\n"
+      "      G_OBJECT_CLASS (" class_name "_parent_class)->constructed;\n"
+      "    " ClassName " *self = " NAMESPACE "_" NAME " (object);\n"
+      (if (string= "y" private) 
+	  (concat   "    " ClassName "Private *priv = self->priv;\n"
+		    "\n"
+		    "    /* Make compiler happy */\n"
+		    "    (void)priv;\n")
+	"")
+      "\n"
+      "  if (chain_up != NULL)\n"
+      "    chain_up (object);\n"
+      "\n"
+      "}"
+      "\n"
+      "static void\n"
+      class_name "_dispose (GObject *object)\n"
+      "{\n"
+      "    " ClassName " *self = (" ClassName " *)object;\n"
+      (if (string= "y" private) 
+	  (concat   "    " ClassName "Private *priv = self->priv;\n")
+	"")
+      "\n"
+      (if (string= "y" private) 
+	  (concat  "    if (priv->dispose_has_run)\n"
+		   "        return;\n"
+		   "\n"
+		   "    priv->dispose_has_run = TRUE;\n"
+		   "\n")
+	"")
+      "    G_OBJECT_CLASS (" class_name "_parent_class)->dispose (object);\n"
+      "}\n"
+      "\n"
+      "static void\n"
+      class_name "_finalize (GObject *object)\n"
+      "{\n"
+      "    " ClassName " *self = (" ClassName " *)object;\n"
+      "\n"
+      "    /* Make compiler happy */\n"
+      "    (void)self;\n"
+      "\n"
+      "    G_OBJECT_CLASS (" class_name "_parent_class)->finalize (object);\n"
+      "}\n"
+      "\n"
+      )
+     )
+    )
+  )
+
+(defun gobject-class-generate (class_name parent_class_name copyright private properties signals)
+  "Generate header (.h) and code (.c) files for a GObject derived class.
+
+It takes 2 parameters:
+   CLASS_NAME: class name, like 'gtk_button' or 'gtk_tree_view'. First
+      element before the underscore character (_) will be used as name
+      space. Example: 'gtk_button' is the 'button' class in 'gtk' name
+      space.
+   PARENT_CLASS_NAME: parent class name, like 'g_object'. First element
+      before the underscore character (_) will be used as name space.
+      Example: 'g_object' is the 'object' class in 'g' namespace.
+"
+  (interactive "sClass name (ie: gtk_tree_view): \nsParent class name (default: g_object): \nsCopyright information : \nsAdd Private structure ([y]/n)?\nsAdd properties ([y]/n)?\nsAdd signals ([y]/n)?\n")
+
+  (let* ((parent_class_name (if (string= "" parent_class_name)
+				"g_object"
+			      (downcase parent_class_name)))
+	 (class_name        (downcase (if (string= "" class_name)
+					  (ask-value-non-empty
+					   "You must provide class name (ie: gtk_tree_view): ")
+					class_name)))
+	 (private (if (string= "n" private) "n" "y"))
+	 (properties (if (string= "n" properties) "n" "y"))
+	 (signals (if (string= "n" signals) "n" "y"))
+	 (copyright_line (if (string= "" copyright)
+			     ""
+			   (concat " * Copyright (C) 2010 " copyright "\n\n")))
+	 (pieces-class_name  (split-string (downcase class_name) "_"))
+	 (pieces-parent_class_name (split-string parent_class_name "_"))
+	 (namespace       (car-safe pieces-class_name))
+	 (name            (string-join (cdr-safe pieces-class_name) "_"))
+	 (NameSpace       (capitalize namespace))
+	 (Name            (mapconcat 'capitalize (cdr-safe pieces-class_name) ""))
+	 (ClassName       (concat NameSpace Name))
+	 (base_file_name (string-join pieces-class_name "-"))
+	 (file_header (concat base_file_name ".h"))
+	 (file_code   (concat base_file_name ".c"))
+	 (DEFINE_NAME (concat (upcase class_name) "_H"))
+	 (parent_include (if (string= "g_object" parent_class_name)
+			     "glib-object.h"
+			   (if (string-match "^gtk_" parent_class_name)
+			       "gtk/gtk.h"
+			     (concat (string-join pieces-parent_class_name "-") ".h")
+			     )
+			   ))
+	 )
+
+    (delete-other-windows)
+    (split-window-vertically)
+
+    (find-file file_header)
+    (insert
+     (concat
+      "/*\n"
+      "* " file_header " - Header for " ClassName "\n"
+      " *\n"
+      copyright_line
+      " * This library is free software; you can redistribute it and/or\n"
+      " * modify it under the terms of the GNU Lesser General Public\n"
+      " * License as published by the Free Software Foundation; either\n"
+      " * version 2.1 of the License, or (at your option) any later version.\n"
+      " *\n"
+      " * This library is distributed in the hope that it will be useful,\n"
+      " * but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+      " * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU\n"
+      " * Lesser General Public License for more details.\n"
+      " *\n"
+      " * You should have received a copy of the GNU Lesser General Public\n"
+      " * License along with this library; if not, write to the Free Software\n"
+      " * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA\n"
+      " */\n"
+      "\n"
+      "#ifndef __" DEFINE_NAME "__\n"
+      "#define __" DEFINE_NAME "__\n"
+      "\n"
+      "#include <" parent_include ">\n"
+      "\n"))
+    (gobject-class-header class_name parent_class_name private)
+    (insert "#endif /* __" DEFINE_NAME "__ */\n")
+
+    (indent-region (point-min) (point-max))
+
+    (other-window 1)
+
+    (find-file file_code)
+    (insert
+     (concat
+      "/*\n"
+      "* " file_code " - Source for " ClassName "\n"
+      " *\n"
+      copyright_line
+      " * This library is free software; you can redistribute it and/or\n"
+      " * modify it under the terms of the GNU Lesser General Public\n"
+      " * License as published by the Free Software Foundation; either\n"
+      " * version 2.1 of the License, or (at your option) any later version.\n"
+      " *\n"
+      " * This library is distributed in the hope that it will be useful,\n"
+      " * but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+      " * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU\n"
+      " * Lesser General Public License for more details.\n"
+      " *\n"
+      " * You should have received a copy of the GNU Lesser General Public\n"
+      " * License along with this library; if not, write to the Free Software\n"
+      " * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA\n"
+      " */\n"
+      "\n"
+      "#include \"" file_header "\"\n"
+      "\n"))
+    (gobject-class-code class_name parent_class_name private properties signals)
+    (indent-region (point-min) (point-max))
+    )
+  )
+
+(provide 'gobject-class)
+
+;;; gobject-class.el ends here
