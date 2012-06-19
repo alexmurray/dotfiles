87c7eec4d556dba42b61cb157ed291e3255d0af6
Alex Murray <murray.alex@gmail.com>
2012-06-19 22:13:33 +0930

    No need for custom keybinding for yasnippet
    Either use TAB if not inside auto-complete OR
    Use ENTER if inside auto-complete

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -365,8 +365,6 @@
 ;; yasnippet
 (add-to-list 'load-path "~/.emacs.d/vendor/yasnippet")
 (require 'yasnippet)
-;; use C-tab so easy to trigger with auto-complete
-(setq yas/trigger-key "<C-tab>")
 (setq yas/snippet-dirs "~/.emacs.d/vendor/yasnippet/snippets")
 ;; enable yasnippet globally
 (yas/global-mode 1)
